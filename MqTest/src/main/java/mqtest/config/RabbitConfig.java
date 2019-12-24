c9d040ea54729e723b9b60e51d5e7e1d6830698e
zhangjunjie-hub <960057926@qq.com>
2019-12-25 01:29:01 +0800

    mq消息队列

---
+++ MqTest/src/main/java/mqtest/config/RabbitConfig.java
@@ -0,0 +1,74 @@
+package mqtest.config;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:config.properties")
+public class RabbitConfig {
+
+    //默认交换机
+    public static final String EXCHANGE_NAME_DEFAULT = "spring-boot-exchange-default";
+    @Value("${rabbitMq.hostName}")
+    private String hostName;
+    @Value("${rabbitMq.port}")
+    private int port;
+    @Value("${rabbitMq.userName}")
+    private String userName;
+    @Value("${rabbitMq.password}")
+    private String password;
+
+    @Bean
+    @Primary
+    public ConnectionFactory connectionFactory(){
+        CachingConnectionFactory  connectionFactory = new CachingConnectionFactory(hostName,port);
+        if(StringUtils.isNotBlank(userName)){
+          connectionFactory.setUsername(userName);
+        }
+        if(StringUtils.isNotBlank(password)){
+            connectionFactory.setPassword(password);
+        }
+        connectionFactory.setVirtualHost(RabbitConfig.EXCHANGE_NAME_DEFAULT);
+        return connectionFactory;
+    }
+
+    @Bean
+    public TopicExchange topicExchange(){
+        TopicExchange topic = new TopicExchange(RabbitAdmin.DEFAULT_EXCHANGE_NAME);
+        topic.setShouldDeclare(false);
+        return topic;
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(){
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin(){
+        RabbitAdmin admin = new RabbitAdmin(connectionFactory());
+        admin.declareExchange(topicExchange());
+        return admin;
+    }
+
+    @Override
+    public String toString() {
+        return "RabbitConfig{" +
+                "hostName='" + hostName + '\'' +
+                ", port=" + port +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ MqTest/src/main/java/mqtest/service/OrderServiceImpl.java
@@ -0,0 +1,29 @@
+package mqtest.service;
+
+import mqtest.config.RabbitConfig;
+import mqtest.entity.OrderSuccessMessage;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl {
+    @Autowired
+    @Qualifier("amqpTemplate")
+    private AmqpTemplate amqpTemplate;
+    public static final String EXCHANGE_NAME_DEFAULT = "spring-boot-exchange-default";
+    public static final String Order_ROUTING_KEY = "order_routing_key";
+    public void send(){
+        OrderSuccessMessage msg = new OrderSuccessMessage();
+        for(int i=0;i<100;i++){
+           msg.setMsgCode(Integer.toString(i));
+           msg.setMsgContent("this is the "+i+"th messages.");
+           msg.setMsgStatus("0");
+           String message = msg.toString();
+           amqpTemplate.convertAndSend(EXCHANGE_NAME_DEFAULT,Order_ROUTING_KEY,message);
+        }
+
+    }
+
+}
