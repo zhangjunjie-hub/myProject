de92baeadda13497d2f179ba7f6328df493e052c
zhangjunjie-hub <960057926@qq.com>
2020-01-21 14:30:04 +0800

    springBoot定时任务创建

---
+++ quartzModule/src/main/java/com/main/job/TestJob.java
@@ -0,0 +1,17 @@
+package com.main.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class TestJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        String printTime = new SimpleDateFormat("yy-MM-dd HH-mm-ss").format(new Date());
+        System.out.println("PrintWordsJob start at:" + printTime + ", prints: Hello Job-" + new Random().nextInt(100));
+    }
+}
