61ca506ddf676837529d3ca20d1d2b0c24c339cb
zhangjunjie-hub <960057926@qq.com>
2020-01-21 14:34:43 +0800

    springBoot定时任务创建

--- demo/src/main/java/com/example/demo/dao/JobAndTriggerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import java.util.List;
-
-import com.example.demo.entity.JobAndTrigger;
-
-public interface JobAndTriggerMapper {
-	public List<JobAndTrigger> getJobAndTriggerDetails();
-}
